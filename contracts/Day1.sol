a28d2fa6465e4a61941f000aff1a6a323ff166dc
kshitijofficial <52281964+kshitijofficial@users.noreply.github.com>
2022-04-26 11:12:27 +0530

    Update Day1.sol

--- contracts/Day1.sol
+++ contracts/Day1.sol
@@ -2,13 +2,5 @@
 pragma solidity >=0.5.0 <0.9.0;
 
 contract Day1 {
-    function expression(uint256 x, uint256 n) public pure returns (uint256) {
-        uint256 total = 1;
-        uint256 multi = x;
-        for (uint256 i = 1; i < n; i++) {
-            total = total + multi;
-            multi = multi * x;
-        }
-        return total;
-    }
+  
 }
